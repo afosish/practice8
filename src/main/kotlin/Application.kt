337b59d9f56461d6fadb770befbc03a3699cb9a4
Aksyme ./ <wwznoz66f@gmail.com>
2025-11-12 16:29:21 +0200

    P3. Rest API

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,13 @@
+package com.example
+
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
